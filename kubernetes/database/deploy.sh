b32a42ff647102b2e383cb3d661f17b3ef0b1178
Bloc De Notas <luarana631@gmail.com>
2025-02-10 14:38:50 -0600

    setting up kubernetes database

--- kubernetes/database/deploy.sh
+++ kubernetes/database/deploy.sh
@@ -2,7 +2,5 @@
 
 kubectl apply -f namespace.yml
 kubectl apply -f mongo-secret.yml
-kubectl apply -f mongo-deployment.yml
-kubectl apply -f mongo-service.yml
-kubectl apply -f mongo-express-deployment.yml
-kubectl apply -f mongo-express-service.yml
+kubectl apply -f mongo.yml
+kubectl apply -f mongo-express.yml
--- kubernetes/database/destroy.sh
+++ kubernetes/database/destroy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-kubectl delete -f mongo-express-service.yml
-kubectl delete -f mongo-express-deployment.yml
-kubectl delete -f mongo-service.yml
-kubectl delete -f mongo-deployment.yml
+kubectl delete -f mongo-express.yml
+kubectl delete -f mongo.yml
 kubectl delete -f mongo-secret.yml
 kubectl delete -f namespace.yml
